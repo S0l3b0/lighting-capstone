4e8d5bfbd41199c01c2b8f4cc2963e9d013167d8
corvidbonez <fbillie@icloud.com>
2026-03-06 15:36:02 -0800

    login_update

--- Lighting-Capstone/src/App.jsx
+++ Lighting-Capstone/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/navbar.jsx';
 import Home from './components/Home.jsx';
+import LoginForm from './login/loginForm.jsx'
 
 function App() {
   return (
-    <div>
+    <div className='h-screen space-y 10'>
       <Navbar />
       <Home />
+      <LoginForm />
     </div>
   )
 }
