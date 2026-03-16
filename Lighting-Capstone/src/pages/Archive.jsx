6f9d3f93afd5b75d549ac89d6b67c5db7b176a37
corvidbonez <fbillie@icloud.com>
2026-03-16 15:05:19 -0700

    archive work

--- Lighting-Capstone/src/pages/Archive.jsx
+++ Lighting-Capstone/src/pages/Archive.jsx
@@ -2,6 +2,8 @@ function Archive() {
     return (
         <div className="h-screen w-screen">
             <h1>Archive Page</h1>
+            <a href="#"><img src="https://tinyurl.com/24r2f89c" alt="exampleimg" /></a>
+            
         </div>
     );
 }
