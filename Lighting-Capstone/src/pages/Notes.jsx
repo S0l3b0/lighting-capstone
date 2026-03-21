a3f119c3bb789effaec55c4079c42171643e37ce
lilyalessi <waterlily.4268@gmail.com>
2026-03-21 16:31:50 -0700

    First draft of notes page

--- Lighting-Capstone/src/pages/Notes.jsx
+++ Lighting-Capstone/src/pages/Notes.jsx
@@ -1,12 +1,30 @@
+import Note from "/src/components/note.jsx";
+
 function Notes() {
     return (
+  
     <div className="h-screen w-screen"><h1>Notes Page</h1>
+    <div className = "flex flex-row gap-4 p-4">
     <Note
       topic = "Broken Cyc Lights"
       text = "Most of our cyc lights need repairs. We should talk to Mr. Fern soon."
       name = "Lily Alessi"
       date = "3/19"
     />
+
+    <Note
+      topic = "Topic"
+      text = "Text/Note Content"
+      name = "Name"
+      date = "Date"
+    />
+
+   {/*Fake button for now*/}
+    <span className = "h-20 w-20 mt-25 ml-15 text-xl ">
+    <button>+</button>
+    </span>
+
+    </div>
     </div>
   );
   };
