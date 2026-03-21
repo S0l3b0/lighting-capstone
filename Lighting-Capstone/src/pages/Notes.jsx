a2f3386b94d959abac0d0de77cdce8f1d25c8684
lilyalessi <waterlily.4268@gmail.com>
2026-03-21 14:18:56 -0700

    Notes component/template made

--- Lighting-Capstone/src/pages/Notes.jsx
+++ Lighting-Capstone/src/pages/Notes.jsx
@@ -1,7 +1,15 @@
 function Notes() {
     return (
-    <div className="h-screen w-screen"><h1>Notes Page</h1></div>);
-  }
+    <div className="h-screen w-screen"><h1>Notes Page</h1>
+    <Note
+      topic = "Broken Cyc Lights"
+      text = "Most of our cyc lights need repairs. We should talk to Mr. Fern soon."
+      name = "Lily Alessi"
+      date = "3/19"
+    />
+    </div>
+  );
+  };
   
   export default Notes;
   
